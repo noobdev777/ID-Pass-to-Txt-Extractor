86d71a34fdbba76d9bcff4facacdbc6bff42fbc2
noobdev777 <164876911+noobdev777@users.noreply.github.com>
2024-05-28 13:01:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+api_id =27665762
+api_hash = "b2b6f18e1280f194b8f7349db4737eb9"
+bot_token = "6997451043:AAF2VPIq3kmAYCBybsn5ygn_EajgQrvn1IM"
+auth_users = "7152249320"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = "7152249320"
 owner_users = [int(num) for num in osowner_users.split(",")]
